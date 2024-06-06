efe60b1768701bef94be2cec67772b66c5ace657
agracio <antonas.graciovas@outlook.com>
2024-06-06 22:15:22 +0100

    merging code changes from edge-js

--- tools/test.js
+++ tools/test.js
@@ -11,8 +11,8 @@ if (!process.env.EDGE_USE_CORECLR) {
 	if (process.platform !== 'win32') {
 		buildParameters = buildParameters.concat(['-sdk:4.5']);
 	}
-
-	run(process.platform === 'win32' ? 'csc' : 'mcs', buildParameters, runOnSuccess);
+    var compiler = 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\csc.exe'
+	run(process.platform === 'win32' ? compiler : 'mcs', buildParameters, runOnSuccess);
 }
 
 else {
