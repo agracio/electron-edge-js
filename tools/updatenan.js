977622094753c7edeba52e3e32e7e8d1c1dec460
Georgi Davidkov <gdavidkov@users.noreply.github.com>
2024-11-06 21:35:32 +0200

    nan can be installed in the top level node_modules

--- tools/updatenan.js
+++ tools/updatenan.js
@@ -1,9 +1,9 @@
 const spawn = require('child_process').spawn
+const existsSync = require('fs').existsSync
 
 if (process.platform === 'win32') {
-    spawn('powershell', ['-Command', "(Get-Content -Raw ./node_modules/nan/nan.h) -replace '#include \"nan_scriptorigin.h\"', '// #include \"nan_scriptorigin.h\"' | Out-File -Encoding Utf8 ./node_modules/nan/nan.h"], { stdio: 'inherit'})
-}else{
-    spawn('sed', ['-i', '-e', 's/^#include .nan_scriptorigin\\.h./\\/\\/ #include nan_scriptorigin.h/', './node_modules/nan/nan.h'], { stdio: 'inherit'})
+    const nanPath = existsSync('./node_modules/nan/nan.h') ? './node_modules/nan/nan.h' : '../nan/nan.h'
+    spawn('powershell', ['-Command', `(Get-Content -Raw ${nanPath}) -replace '#include \"nan_scriptorigin.h\"', '// #include \"nan_scriptorigin.h\"' | Out-File -Encoding Utf8 ${nanPath}`], { stdio: 'inherit' })
+} else {
+    spawn('sed', ['-i', '-e', 's/^#include .nan_scriptorigin\\.h./\\/\\/ #include nan_scriptorigin.h/', './node_modules/nan/nan.h'], { stdio: 'inherit' })
 }
-
-
