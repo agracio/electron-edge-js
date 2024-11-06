94dec016d7cb06ff404e03b128ce67cd6de45257
Georgi Davidkov <gdavidkov@users.noreply.github.com>
2024-11-06 21:39:17 +0200

    apply the fix not only for win32

--- tools/updatenan.js
+++ tools/updatenan.js
@@ -1,9 +1,8 @@
 const spawn = require('child_process').spawn
 const existsSync = require('fs').existsSync
-
+const nanPath = existsSync('./node_modules/nan/nan.h') ? './node_modules/nan/nan.h' : '../nan/nan.h'
 if (process.platform === 'win32') {
-    const nanPath = existsSync('./node_modules/nan/nan.h') ? './node_modules/nan/nan.h' : '../nan/nan.h'
     spawn('powershell', ['-Command', `(Get-Content -Raw ${nanPath}) -replace '#include \"nan_scriptorigin.h\"', '// #include \"nan_scriptorigin.h\"' | Out-File -Encoding Utf8 ${nanPath}`], { stdio: 'inherit' })
 } else {
-    spawn('sed', ['-i', '-e', 's/^#include .nan_scriptorigin\\.h./\\/\\/ #include nan_scriptorigin.h/', './node_modules/nan/nan.h'], { stdio: 'inherit' })
+    spawn('sed', ['-i', '-e', 's/^#include .nan_scriptorigin\\.h./\\/\\/ #include nan_scriptorigin.h/', nanPath], { stdio: 'inherit' })
 }
