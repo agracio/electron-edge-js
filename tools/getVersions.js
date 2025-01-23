23f1863b088b5e5c29ae418bc3eb7f89f240cd0a
agracio <agracio@users.noreply.github.com>
2025-01-23 00:31:36 +0000

    updating workflow

--- tools/getVersions.js
+++ tools/getVersions.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const http = require('isomorphic-git/http/web');
 const git = require("isomorphic-git");
-const { get } = require("https");
 
 const majors = [29, 30, 31, 32, 33, 34];
 
@@ -10,35 +9,41 @@ git.getRemoteInfo({
     //corsProxy: "https://cors.isomorphic-git.org",
     url: "https://github.com/electron/electron"
 }).then(info =>{
-    let result = Object.keys(info.refs.tags);
+
+    let tags = Object.keys(info.refs.tags);
     let oses = ['macos-13', 'macos-14', 'ubuntu-22.04', 'ubuntu-22.04-arm', 'windows-2022'];
     let versions = [];
+    let results = [];
+
     majors.forEach((major) => {
-        
-        oses.forEach((os) => {
-            versions.push({'electron': getVersion(result, major), 'os': `${os}`});
+        versions.push(getVersion(tags, major));
+    });
+
+    oses.forEach((os) => {
+        versions.forEach((version) => {
+            results.push({'electron': `${version}`, 'os': `${os}`});
         });
     });
-    let res = `{'include':${JSON.stringify(versions)}}`
+
+    let res = `{'include':${JSON.stringify(results)}}`
     fs.writeFileSync('electron-versions.txt', res);
     console.log(res);
 });
 
-function getVersion(result, major){
-    result = result
+function getVersion(tag, major){
+    tag = tag
     .filter(function (str) { return !str.includes('^'); })
     .filter(function (str) { return !str.includes('-'); })
     .filter(function (str) { return str.startsWith(`v${major}.`); })
     .sort()
     .reverse();
 
-    if(result.length !== 0){
-        return result[0].replace('v', '')      
+    if(tag.length !== 0){
+        return tag[0].replace('v', '')      
     }
     else{
         throw `Unable to resolve latest version for Electron ${major}`
     }
-
 }
 
 
