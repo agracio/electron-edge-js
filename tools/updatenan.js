01e2d0701266be3b5397dec082f41ec24d199a20
agracio <agracio@users.noreply.github.com>
2025-03-02 16:41:55 +0000

    removing unused script

--- tools/updatenan.js
+++
@@ -1,6 +0,0 @@
-const spawn = require('child_process').spawn
-const existsSync = require('fs').existsSync
-const nanPath = existsSync('./node_modules/nan/nan.h') ? './node_modules/nan/nan.h' : '../nan/nan.h'
-if (process.platform !== 'win32' && existsSync(nanPath)) {
-    spawn('sed', ['-i', '-e', 's/^#include .nan_scriptorigin\\.h./\\/\\/ #include nan_scriptorigin.h/', nanPath], { stdio: 'inherit' })
-}
