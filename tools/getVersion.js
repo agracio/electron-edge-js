e7e63c923964c3843c9ab6a8ba6d5b8c365b8dc0
agracio <agracio@users.noreply.github.com>
2026-08-25 19:22:46 +0100

    updating Ubuntu runners

--- tools/getVersion.js
+++ tools/getVersion.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { execSync } = require('child_process');
 
 const majors = [39, 40, 41, 42, 43, 44];
-const oses = ['macos-15-intel', 'macos-15', 'ubuntu-22.04', 'ubuntu-22.04-arm', 'windows-2025', 'windows-11-arm'];
+const oses = ['macos-15-intel', 'macos-15', 'ubuntu-24.04', 'ubuntu-24.04-arm', 'windows-2025', 'windows-11-arm'];
 // const oses = ['ubuntu-22.04', 'ubuntu-22.04-arm'];
 
 getVersion();
